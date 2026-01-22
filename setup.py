5cca23bb5ade5f1cd8bb8371062d7f5aadacb0dc
Mohammad Farooque Ahmed <2306033@kiit.ac.in>
2026-01-22 17:09:23 +0530

    Exception handling and logging done

--- setup.py
+++ setup.py
@@ -21,5 +21,12 @@ def get_requirements()->List[str]:
         
     return requirement_lst
 
-print(get_requirements())
+setup(
+    name="NetworkSecurity-1",
+    version="0.0.1",
+    author="Mohammad Farooque Ahmed",
+    author_email="2306033@kiit.ac.in",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
     
