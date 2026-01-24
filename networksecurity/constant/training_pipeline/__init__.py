a908d7068c0ebe1e9324968c393fccf7a42ca5e7
Mohammad Farooque Ahmed <2306033@kiit.ac.in>
2026-01-24 17:45:29 +0530

    Data Transformation Done

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -29,3 +31,26 @@ DATA_INGESTION_TRAIN_FILE_NAME: str = "train.csv"
 DATA_INGESTION_TEST_FILE_NAME: str = "test.csv"
 DATA_INGESTION_FEATURE_FILE_NAME: str = "phisingData.csv"
 DATA_INGESTION_TRAIN_TEST_SPLIT: float = 0.2
+
+#data validation related constant start
+
+DATA_VALIDATION_DIR_NAME:str="data_validation"
+DATA_VALIDATION_VALID_DIR:str="validated"
+DATA_VALIDATION_INVALID_DIR:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+## kkn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
